a2866c9f2ee97ba5ae42ee1e072e48a9eb2c1d3a
DmytryM <28marchuk28@i.ua>
2022-03-24 23:56:49 +0200

    Tasks

--- py_advanced/4.2.py
+++ py_advanced/4.2.py
@@ -1,3 +1,10 @@
-list1 = [[1] * 3] * 3
-list1[0][1] = 5
-print(*list1)
+# list1 = [[1] * 3] * 3
+# list1[0][1] = 5
+# print(*list1)
+
+n = int(input())
+
+lists = [[j for j in range(1, n+1)] for i in range(n)]
+
+for i in lists:
+    print(i)
