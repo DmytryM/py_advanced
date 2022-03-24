7229f30a487b3b388c884c41e1e79beda4b15c54
DmytryM <28marchuk28@i.ua>
2022-03-25 00:48:31 +0200

    Tasks

--- py_advanced/4.2.py
+++ py_advanced/4.2.py
@@ -2,9 +2,27 @@
 # list1[0][1] = 5
 # print(*list1)
 
-n = int(input())
 
-lists = [[j for j in range(1, n+1)] for i in range(n)]
+# ---------------- 1 --------------------
 
-for i in lists:
-    print(i)
+# n = int(input())
+#
+# lists = [[j for j in range(1, n+1)] for i in range(n)]
+#
+# for i in lists:
+#     print(i)
+
+
+# ---------------- 2 --------------------
+
+# n = int(input())
+#
+# lists = [[j for j in range(1, i+1)] for i in range(1, n + 1)]
+#
+# for i in lists:
+#     print(i)
+
+
+# ---------------- 3 --------------------
+
+
