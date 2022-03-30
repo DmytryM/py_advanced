5038c00a9dfda3c8ef1e28e5aefd481dd2eaef07
DmytryM <28marchuk28@i.ua>
2022-03-31 00:06:04 +0300

    Tasks

--- py_advanced/4.4.py
+++ py_advanced/4.4.py
@@ -104,16 +104,46 @@
 
 '''Напишите программу, которая выводит максимальный элемент в заштрихованной области квадратной матрицы.'''
 
-a = int(input())
-res = [input().split() for _ in range(a)]
-res2 = []
+# a = int(input())
+# res = [input().split() for _ in range(a)]
+# res2 = []
+#
+# for i in range(a):
+#     for j in range(a):
+#         if(i < j and i < a - 1 - j) or (i > j and i > a - 1 - j):
+#             pass
+#         else:
+#             res2.append(int(res[i][j]))
+#
+# print(max(res2))
 
-for i in range(a):
-    for j in range(a):
-        if(i < j and i < a - 1 - j) or (i > j and i > a - 1 - j):
-            pass
-        else:
-            res2.append(int(res[i][j]))
+# ---------------- 7 --------------------
 
-print(max(res2))
+'''Квадратная матрица разбивается на четыре четверти, ограниченные главной и побочной диагоналями: верхнюю, 
+нижнюю, левую и правую'''
+
+# n = int(input())
+# matrix = []
+# quadrants = [['Верхняя четверть:', 0],
+#              ['Правая четверть:', 0],
+#              ['Нижняя четверть:', 0],
+#              ['Левая четверть:', 0]]
+#
+# for _ in range(n):
+#     row = [int(i) for i in input().split()]
+#     matrix.append(row)
+#
+# for i in range(n):
+#     for j in range(n):
+#         if i < j and i + j + 1 < n :
+#             quadrants[0][1] += matrix[i][j]
+#         elif i < j and i + j + 1 > n:
+#             quadrants[1][1] += matrix[i][j]
+#         elif i > j and i + j + 1 > n:
+#             quadrants[2][1] += matrix[i][j]
+#         elif i > j and i + j + 1 < n:
+#             quadrants[3][1] += matrix[i][j]
+#
+# for i in range(4):
+#     print(quadrants[i][0], quadrants[i][1])
 
