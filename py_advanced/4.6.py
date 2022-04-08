bfd733294357d2ac0092343e18f27916e9ca92f0
DmytryM <28marchuk28@i.ua>
2022-04-08 23:54:36 +0300

    Tasks

--- py_advanced/4.6.py
+++ py_advanced/4.6.py
@@ -4,8 +4,19 @@
 mn×m, заполнив её символами . и * в шахматном порядке. В левом верхнем углу должна стоять точка. Выведите полученную
 матрицу на экран, разделяя элементы пробелами.'''
 
-n, m = map(int, input().split())
-for i in range(n):
-    row = ['.' if (i + j) % 2 == 0 else '*' for j in range(m)]
-    print(*row)
+# n, m = map(int, input().split())
+# for i in range(n):
+#     row = ['.' if (i + j) % 2 == 0 else '*' for j in range(m)]
+#     print(*row)
 
+# ---------------- 2 --------------------
+
+a = int(input())
+
+for i in range(a):  # заполняем главную диагональ единицами, а побочную двойками
+    # matrix[i][a - i - 1] = 1
+    # for j in range(a):
+    #     if i > a - 1 - j:
+    #         matrix[i][j] = 2
+    matrix = [0 if i < a - 1 - j else 1 if i == a - 1 - j else 2 for j in range(a)]
+    print(*matrix)
