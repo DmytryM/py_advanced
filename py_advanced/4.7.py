3bc349a0b88bfd5a9f0075bb36f5846ed9b2a72a
DmytryM <28marchuk28@i.ua>
2022-04-11 23:43:29 +0300

    Tasks

--- py_advanced/4.7.py
+++ py_advanced/4.7.py
@@ -1,10 +1,20 @@
 # ---------------- 1 --------------------
 
-a, b = map(int, input().split())
-res1 = [[int(j) for j in input().split()] for i in range(a)]
-input()
-res2 = [[int(j) for j in input().split()] for i in range(a)]
+# a, b = map(int, input().split())
+# res1 = [[int(j) for j in input().split()] for i in range(a)]
+# input()
+# res2 = [[int(j) for j in input().split()] for i in range(a)]
+#
+# for i in range(a):
+#     row = [res1[i][j] + res2[i][j] for j in range(b)]
+#     print(*row)
 
-for i in range(a):
-    row = [res1[i][j] + res2[i][j] for j in range(b)]
-    print(*row)
+# ---------------- 2 --------------------
+
+# a, b = map(int, input().split())
+# res1 = [[int(j) for j in input().split()] for i in range(a)]
+#
+# a1, b1 = map(int, input().split())
+# res2 = [[int(j) for j in input().split()] for i in range(a1)]
+
+
