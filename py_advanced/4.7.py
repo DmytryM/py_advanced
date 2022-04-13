1c39c168ed25139870bfb43141412f5e9d5996c9
DmytryM <28marchuk28@i.ua>
2022-04-13 14:04:35 +0300

    Tasks

--- py_advanced/4.7.py
+++ py_advanced/4.7.py
@@ -29,17 +29,31 @@
 
 # ---------------- 3 --------------------
 
-a = int(input())
-res1 = [[int(j) for j in input().split()] for i in range(a)]
-degree = int(input())
-res2 = res1.copy()
-res3 = 0
-for s in range(degree - 1):
-    res3 = [[0 for _ in range(a)] for _ in range(a)]
-    for i in range(a):
-        for j in range(a):
-            for x in range(a):
-                res3[i][j] += res1[i][x] * res2[x][j]
-    res2 = res3.copy()
+# a = int(input())
+# res1 = [[int(j) for j in input().split()] for i in range(a)]
+# degree = int(input())
+# res2 = res1.copy()
+# res3 = 0
+# for s in range(degree - 1):
+#     res3 = [[0 for _ in range(a)] for _ in range(a)]
+#     for i in range(a):
+#         for j in range(a):
+#             for x in range(a):
+#                 res3[i][j] += res1[i][x] * res2[x][j]
+#     res2 = res3.copy()
+#
+# [print(*res) for res in res3]
 
-[print(*res) for res in res3]
+# ---------------- 4 --------------------
+
+# tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
+# new_tuples = []
+# for i in range(len(tuples)):
+#     if len(tuples[i]) != 0:
+#         a = list(tuples[i])
+#         a[-1] = 100
+#         tuples[i] = tuple(a)
+#         new_tuples.append(tuples[i])
+# print(new_tuples)
+
+# ---------------- 5 --------------------
