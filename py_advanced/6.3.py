de2de81341452a673131ba11f64d007089e8be95
DmytryM <28marchuk28@i.ua>
2022-04-14 00:11:31 +0300

    Tasks

--- py_advanced/6.3.py
+++ py_advanced/6.3.py
@@ -15,6 +15,29 @@
 
 # ---------------- 3 --------------------
 
-numbers = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4), (90, 10))
+# numbers = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4), (90, 10))
+#
+# print([sum(i) / len(i) for i in numbers])
 
-print([sum(i) / len(i) for i in numbers])
+# ---------------- 4 --------------------
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# resX = (-b) / (2 * a)
+# resY = ((4 * a * c) - (b ** 2)) / (4 * a)
+#
+# res = (resX, resY)
+#
+# print(res)
+
+# ---------------- 5 --------------------
+
+n = int(input())
+pupil = tuple(input().split() for _ in range(n))
+[print(*i) for i in pupil]
+print()
+[print(*i) for i in pupil if int(i[1]) > 3]
+
+
