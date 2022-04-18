69cc8541d113b5faa580e22120e6c6a5deefb18c
DmytryM <28marchuk28@i.ua>
2022-04-18 23:36:02 +0300

    Tasks

--- py_advanced/10.3.py
+++ py_advanced/10.3.py
@@ -5,9 +5,19 @@
 
 # ---------------- 2 --------------------
 
-dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
-dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+# dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+# dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+#
+# dict1.update(dict2)
+# result = {dict1}
+# print(result)
 
-dict1.update(dict2)
-result = {dict1}
+# ---------------- 3 --------------------
+
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+letters = [c for c in text]
+result = {}
+for let in letters:
+    result[let] = result.get(let, 0) + 1
+
 print(result)
