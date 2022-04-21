60b4ef141fe0474c7adcf06a817a3dc83ac8ee83
DmytryM <28marchuk28@i.ua>
2022-04-22 00:58:29 +0300

    Tasks

---
+++ py_advanced/10.4.py
@@ -0,0 +1,20 @@
+# ---------------- 1 --------------------
+
+# dictionary = {}
+# keys = []
+# words = []
+#
+# for i in range(int(input())):
+#     pair = input().split(': ')
+#     keys.append(pair[0].lower())
+#     words.append(pair[1].strip())
+#
+# dictionary = dict(zip(keys, words))
+#
+# for i in range(int(input())):
+#     quest = input().lower()
+#     final = dictionary.get(quest, "Не найдено")
+#     print(final)
+
+# ---------------- 2 --------------------
+
