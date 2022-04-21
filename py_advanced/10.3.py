aabe773762c3b0b6a927136c194e7d8bafea0461
DmytryM <28marchuk28@i.ua>
2022-04-21 19:24:08 +0300

    Tasks

--- py_advanced/10.3.py
+++ py_advanced/10.3.py
@@ -88,4 +88,12 @@
 
 # ---------------- 7 --------------------
 
-
+# a = input().split()
+# res = []
+# for i in a:
+#     if i not in res:
+#         res.append(i)
+#         print(i, end=' ')
+#     else:
+#         print(f"{i}_{res.count(i)}", end=' ')
+#         res.append(i)
