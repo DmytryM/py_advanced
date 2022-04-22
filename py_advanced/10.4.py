9bfb90341f6190feaf3611d729bb0df4494aa34b
DmytryM <28marchuk28@i.ua>
2022-04-22 22:26:27 +0300

    Tasks

--- py_advanced/10.4.py
+++ py_advanced/10.4.py
@@ -26,3 +26,58 @@
 #     result2[let] = result2.get(let, 0) + 1
 #
 # print('YES' if result1 == result2 else 'NO')
+
+# ---------------- 3 --------------------
+
+# def compare(a, b):
+#     result1 = {}
+#     result2 = {}
+#
+#     for let in a:
+#         if let.isalpha():
+#             result1[let] = result1.get(let, 0) + 1
+#     for let in b:
+#         if let.isalpha():
+#             result2[let] = result2.get(let, 0) + 1
+#
+#     print('YES' if result1 == result2 else 'NO')
+#
+#
+# a = input().lower()
+# b = input().lower()
+# compare(a, b)
+
+# ---------------- 4 --------------------
+
+# res = {}
+# for i in range(int(input())):
+#     key, value = input().split()
+#     res[key] = value
+#     res[value] = key
+#
+# print(res[input()])
+
+# ---------------- 5 --------------------
+
+# res = {}
+# for i in range(int(input())):
+#     country, *city = input().split()
+#     res[country] = city
+# for i in range(int(input())):
+#     find = input()
+#     for key, values in res.items():
+#         if find in values:
+#             print(key)
+
+# ---------------- 6 --------------------
+
+# result = {}
+# for i in range(int(input())):
+#     number, name = input().split()
+#     result.setdefault(name, []).append(number)
+# for i in range(int(input())):
+#     quest = input().capitalize()
+#     print(*result.get(quest, ["абонент не найден"]))
+
+# ---------------- 7 --------------------
+
