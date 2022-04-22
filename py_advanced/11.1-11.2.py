a6874e15891a87fccce260811e48c3455bb23a8b
DmytryM <28marchuk28@i.ua>
2022-04-23 01:18:35 +0300

    Tasks

--- py_advanced/11.1-11.2.py
+++ py_advanced/11.1-11.2.py
@@ -1,7 +1,24 @@
 # ---------------- 1 --------------------
 
-import random
+# import random
+#
+# n = int(input())    # количество попыток
+# for i in range(n):
+#     print(random.choice(['Орел','Решка']))
 
-n = int(input())    # количество попыток
-for i in range(n):
-    print(random.choice(['Орел','Решка']))
+# ---------------- 2 --------------------
+
+# import random
+#
+#
+# def password(length):
+#     upper = [chr(_) for _ in range(65, 91)]
+#     lower = [chr(_) for _ in range(97, 123)]
+#     for i in range(length):
+#         yield random.choice(upper+lower)
+#
+#
+# length = int(input())  # длина пароля
+# for i in password(length):
+#     print(i, end='')
+# # print(password(length), end='')
