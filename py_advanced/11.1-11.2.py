0a16be516b3971816f4f0726c91c908875bac8b5
DmytryM <28marchuk28@i.ua>
2022-04-23 20:01:59 +0300

    Tasks

--- py_advanced/11.1-11.2.py
+++ py_advanced/11.1-11.2.py
@@ -33,19 +33,70 @@
 # print(*sorted(a))
 
 # ---------------- 4 --------------------
-import random
+# import random
+#
+#
+# def generate_ip():
+#     # res = []
+#     # for i in range(4):
+#     #     res.append(str(random.randint(0, 255)))
+#     # return '.'.join(res)
+#     return '.'.join(str(random.randint(0, 255)) for _ in range(4))
+#
+#
+# result = generate_ip()
+# print(generate_ip())
+# # result = [str(x) for x in result]
+# # print(result)
+# # print('.'.join(result))
 
+# ---------------- 5 --------------------
+# import random
+# import string
+#
+#
+# def generate_index():
+#     lln = [str(random.choice(string.ascii_uppercase) + (str(random.choice(string.ascii_uppercase))) + str(
+#         random.randint(0, 99))), str(random.randint(0, 99)) + str(random.choice(string.ascii_uppercase)) + str(
+#         random.choice(string.ascii_uppercase))]
+#     return '_'.join(lln)
+#
+#
+# generate = generate_index()
+# print(generate)
 
-def generate_ip():
-    # res = []
-    # for i in range(4):
-    #     res.append(str(random.randint(0, 255)))
-    # return '.'.join(res)
-    return '.'.join(str(random.randint(0, 255)) for _ in range(4))
+# ---------------- 6 --------------------
 
+# import random
+#
+# matrix = [[1, 2, 3, 4],
+#           [5, 6, 7, 8],
+#           [9, 10, 11, 12],
+#           [13, 14, 15, 16]]
+#
+# random.shuffle(matrix)
+# [random.shuffle(i) for i in matrix]
+# [print(*res) for res in matrix]
 
-result = generate_ip()
-print(generate_ip())
-# result = [str(x) for x in result]
-# print(result)
-# print('.'.join(result))
+# ---------------- 7 --------------------
+
+# import random
+#
+# x = [1, 2, 3, 4, 5, 6, 7]
+# res = []
+#
+# while len(res) < 100:
+#     choice = random.sample(x, len(x))
+#     if choice not in res:
+#         res.append(choice)
+# [print(*res, sep='') for res in res]
+# print(f'{i}={*random.sample(x, len(x))}', sep='')
+
+# ---------------- 8 --------------------
+# import random
+#
+# numbers = input()
+#
+# print(*random.sample(numbers, len(numbers)), sep="")
+
+# ---------------- 9 --------------------
