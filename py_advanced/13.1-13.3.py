4aa069e14216a07834bdc338e328a0b948865c8a
DmytryM <28marchuk28@i.ua>
2022-04-29 01:15:20 +0300

    Tasks

--- py_advanced/13.1-13.3.py
+++ py_advanced/13.1-13.3.py
@@ -148,6 +148,16 @@
 
 # ----------------- 14--------------------
 
-numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
-for i in numbers:
-    print()
+# numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j,
+#            1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+# m = max(numbers, key=abs)
+# print(m)
+# print(abs(m))
+
+# ----------------- 15--------------------
+
+# n = int(input())
+# z1, z2 = complex(input()), complex(input())
+# z11, z22 = z1.conjugate(), z2.conjugate()
+# result = z1 ** n + z2 ** n + z11 ** n + z22 ** (n + 1)
+# print(result)
