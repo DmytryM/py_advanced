3f54f5634b1549e144db65be0cfbce84de03337e
DmytryM <28marchuk28@i.ua>
2022-04-30 23:40:55 +0300

    Tasks

--- py_advanced/14.1.py
+++ py_advanced/14.1.py
@@ -29,13 +29,17 @@
 
 # ----------------- 1 --------------------
 
-import turtle
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+def rectangle(width, height):
+    import turtle
+    for i in range(2):
+        turtle.forward(width)
+        turtle.left(90)
+        turtle.forward(height)
+        turtle.left(90)
+
+w = int(input('Введити ширину = '))
+h = int(input('Введити высоту = '))
+
+rectangle(w, h)
 
 
