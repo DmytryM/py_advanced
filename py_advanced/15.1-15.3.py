9456ffb665ea95eabaa4c3303a5c59f64c5149fc
DmytryM <28marchuk28@i.ua>
2022-05-02 11:45:38 +0300

    Tasks

--- py_advanced/15.1-15.3.py
+++ py_advanced/15.1-15.3.py
@@ -31,3 +31,14 @@
 # print(count_args([], (''), 'a', 12, False))
 
 # ----------------- 3 --------------------
+
+# def sq_sum(*args):
+#     return sum([i ** 2 for i in args])
+# print(sq_sum())
+# print(sq_sum(2))
+# print(sq_sum(1.5, 2.5))
+# print(sq_sum(1, 2, 3))
+# print(sq_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+# ----------------- 4 --------------------
+
