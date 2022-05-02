bc01bcc3a429a58be1069c61913479459a7b0dee
DmytryM <28marchuk28@i.ua>
2022-05-02 11:42:44 +0300

    Tasks

--- py_advanced/15.1-15.3.py
+++ py_advanced/15.1-15.3.py
@@ -21,11 +21,13 @@
 
 # ----------------- 2 --------------------
 
-def count_args(*args):
-    return args
+# def count_args(*args):
+#     return len(args)
+#
+#
+# print(count_args())
+# print(count_args(10))
+# print(count_args('stepik', 'beegeek'))
+# print(count_args([], (''), 'a', 12, False))
 
-
-print(count_args())
-print(count_args(10))
-print(count_args('stepik', 'beegeek'))
-print(count_args([], (''), 'a', 12, False))
+# ----------------- 3 --------------------
