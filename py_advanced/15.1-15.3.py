02a56ddf69dec646dfc6a46346ab4be50aaf1113
DmytryM <28marchuk28@i.ua>
2022-05-02 23:23:10 +0300

    Tasks

--- py_advanced/15.1-15.3.py
+++ py_advanced/15.1-15.3.py
@@ -93,3 +93,35 @@
 # info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
 
 # ----------------- 8 --------------------
+
+# def comparator(pair):
+#     return pair[1]
+#
+#
+# pairs = [(5, 4), (3, 2), (1, 7), (8, 2)]
+# pairs.sort(key=comparator)
+# print(pairs)
+
+# ----------------- 9 --------------------
+
+# def mid(number):
+#     return sum(number) / len(number)
+#
+#
+# numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34),
+#            (10, 20, 30, -2),
+#            (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+#
+# print(min(numbers, key=mid))
+# print(max(numbers, key=mid))
+
+# ----------------- 10 --------------------
+
+# def distance(coords):
+#     x, y = coords
+#     return x ** 2 + y ** 2
+#
+#
+# points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
+#
+# print(sorted(points, key=distance))
