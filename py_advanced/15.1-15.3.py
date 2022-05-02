dd0d37fb540ab075a8046b5657642ee772d76e0b
DmytryM <28marchuk28@i.ua>
2022-05-02 12:49:56 +0300

    Tasks

--- py_advanced/15.1-15.3.py
+++ py_advanced/15.1-15.3.py
@@ -85,5 +85,11 @@
 
 # ----------------- 7 --------------------
 
-def info_kwargs():
-    pass
+# def info_kwargs(**kwargs):
+#     for key, value in sorted(kwargs.items()):
+#         print(f"{key}: {value}")
+#
+#
+# info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
+
+# ----------------- 8 --------------------
