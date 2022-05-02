1b056cc8eb05aadc161e51f2606ce3886d4f13f7
DmytryM <28marchuk28@i.ua>
2022-05-02 12:03:03 +0300

    Tasks

--- py_advanced/15.1-15.3.py
+++ py_advanced/15.1-15.3.py
@@ -42,3 +42,19 @@
 
 # ----------------- 4 --------------------
 
+# def mean(*args):
+#     s = [float(i) for i in args if type(i) in (int, float)]
+#     if len(s) > 0:
+#         return sum(s) / len(s)
+#     else:
+#         return 0.0
+#
+#
+# print(mean())
+# print(mean(7))
+# print(mean(1.5, True, ['stepik'], 'beegeek', 2.5, (1, 2)))
+# print(mean(True, ['stepik'], 'beegeek', (1, 2)))
+# print(mean(-1, 2, 3, 10, ('5')))
+# print(mean(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+# ----------------- 5 --------------------
