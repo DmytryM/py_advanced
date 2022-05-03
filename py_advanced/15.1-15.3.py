2e11599d36402b63a255a8d9dfe40ddfbcdfca54
DmytryM <28marchuk28@i.ua>
2022-05-04 00:10:24 +0300

    Tasks

--- py_advanced/15.1-15.3.py
+++ py_advanced/15.1-15.3.py
@@ -210,3 +210,11 @@
 # print(result1)
 
 # ----------------- 15 --------------------
+
+def comparator(n):
+    return sum([int(i) for i in str(n)])
+
+numbers = [int(i) for i in input().split()]
+
+numbers.sort()
+print(*sorted(numbers, key=comparator))
