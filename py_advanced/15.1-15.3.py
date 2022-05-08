c0b113c9c3c55e8c139dea5c93eda2798dd23a7f
DmytryM <28marchuk28@i.ua>
2022-05-08 20:02:41 +0300

    Tasks

--- py_advanced/15.1-15.3.py
+++ py_advanced/15.1-15.3.py
@@ -363,3 +363,20 @@
 # print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
 
 # ----------------- 21 --------------------
+
+# from functools import reduce
+#
+# floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+# words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+# numbers = [4, 6, 9, 23, 5]
+#
+# # Исправьте этот код
+# map_result = list(map(lambda num: round(num**2, 1), floats))
+# filter_result = list(filter(lambda name: name if len(name) > 4 and name == name[::-1] else None, words))
+# reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
+#
+# print(map_result)
+# print(filter_result)
+# print(reduce_result)
+
+# ----------------- 22 --------------------
