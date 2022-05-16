ec59befd7d3887040701eaf518fa9d2235f2ed7b
DmytryM <28marchuk28@i.ua>
2022-05-16 10:37:04 +0300

    Tasks

--- py_advanced/17.1-17.4.py
+++ py_advanced/17.1-17.4.py
@@ -1,11 +1,24 @@
 # ----------------- 1 --------------------
 
-file = open(input(), 'r')
-print(file.read())
-file.close()
+# file = open(input(), 'r')
+# print(file.read())
+# file.close()
 
 # ----------------- 2 --------------------
 
-with open(input()) as output:
-    print(output.readlines()[-2])
-output.close()
+# with open(input()) as output:
+#     print(output.readlines()[-2])
+# output.close()
+
+# ----------------- 3 --------------------
+# import random
+#
+# path = "lines.txt"
+# file = open(path, 'r', encoding='utf-8')
+# languages = [line.strip() for line in file.readlines()]
+# print(random.choice(languages))
+# file.close()
+
+# ----------------- 4 --------------------
+
+print(__import__('random').choice(list(open("lines.txt", encoding='utf-8'))))
