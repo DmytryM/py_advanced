74b91380d581402fbe741e47bf8da23144af1fcc
DmytryM <28marchuk28@i.ua>
2022-07-03 13:03:42 +0300

    Tasks

---
+++ py_advanced/EXAM_1.py
@@ -0,0 +1,22 @@
+# ---------------------------1---------------------------
+
+# m, a = int(input()), input().split()
+# print([a[i::m] for i in range(m)])
+
+# ---------------------------2---------------------------
+
+# a = int(input())
+# matrix = [[int(j) for j in input().split()] for i in range(a)]
+# res = []
+# for i in range(a):
+#     for j in range(a):
+#         if (j >= i >= a - 1 - j) or (i >= j and i >= a - 1 - j):
+#             res.append(matrix[i][j])
+#
+# print(max(res))
+
+# [print(*matrix) for matrix in matrix]
+
+# ---------------------------3---------------------------
+
+
