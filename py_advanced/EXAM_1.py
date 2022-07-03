23f964bd66d143a383bf97a7acc666e2a69d6ed6
DmytryM <28marchuk28@i.ua>
2022-07-03 14:49:37 +0300

    Tasks

--- py_advanced/EXAM_1.py
+++ py_advanced/EXAM_1.py
@@ -19,4 +19,41 @@
 
 # ---------------------------3---------------------------
 
+# # put your python code here
+# n = int(input())
+#
+# matrix = [[int(j) for j in input().split()] for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(n):
+#         print(matrix[j][i], end=' ')
+#     print()
+#
+# # res = zip(*matrix)
+# # [print(*res) for res in res]
 
+# ---------------------------4---------------------------
+
+# first = int(input())
+# matrix = [['.' for _ in range(first)] for _ in range(first)]
+# for i in range(first):
+#     for j in range(first):
+#         if (i == j or i == first - 1 - j) or i == first // 2 or j == first // 2:
+#             matrix[i][j] = '*'
+# [print(*res) for res in matrix]
+
+# ---------------------------5---------------------------
+
+# n = int(input())
+#
+# matrix = [[int(j) for j in input().split()] for _ in range(n)]
+# res = True
+# for i in range(n):
+#     for j in range(n):
+#         if matrix[i][j] != matrix[n - j - 1][n - i - 1]:
+#             res = False
+#             break
+# print('YES' if res else 'NO')
+
+
+# ---------------------------6---------------------------
